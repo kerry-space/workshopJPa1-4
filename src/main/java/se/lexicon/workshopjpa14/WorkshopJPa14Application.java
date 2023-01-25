b2a7b248086773fbcfa60acadeee6c2d8e2a9363
kerry jalal <khariy34@hotmail.com>
2023-01-25 12:32:20 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/workshopjpa14/WorkshopJPa14Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopjpa14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopJPa14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopJPa14Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/workshopjpa14/WorkshopJPa14ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopjpa14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopJPa14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
